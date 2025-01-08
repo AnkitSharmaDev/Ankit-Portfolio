40d458d6d246c944cbfa0b1fa672fe89e043673b
Ankit Sharma <ankitsharma64604@gmail.com>
2025-01-08 13:25:49 +0000

    Footer updated

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import styles from './FooterStyles.module.css' ;
 function Footer() {
   return (
     <section id='footer' className={styles.container}>
-        <p>	&#169; 2024 Ankit Sharma. <br />
+        <p>	&#169; 2025 Ankit Sharma. <br />
         All rights reserved.
         </p>
     </section>
